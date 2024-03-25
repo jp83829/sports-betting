d8c46aa26e36bb455245a95dd2127cb28491989a
jp83829 <162356282+jp83829@users.noreply.github.com>
2024-03-25 16:54:52 -0700

    Update code.R

--- code.R
+++ code.R
@@ -22,13 +22,15 @@ fighter <- fighter_raw %>%
   select(name, date_of_birth)
 
 # standardize variables
-count <- c('R_SIG_STR.', 'B_SIG_STR.', 'R_TOTAL_STR.', 'B_TOTAL_STR.') #character "x of y" to be converted to percentage
+count <- c('R_SIG_STR.', 'B_SIG_STR.', 'R_TOTAL_STR.', 'B_TOTAL_STR.', 'R_TD', 'B_TD') #character "x of y" to be converted to percentage
+time <- c('R_CTRL', 'B_CTRL') #character time span to be converted to numeric
 fight <- fight_raw %>% 
-  mutate_at(vars(count), ~ as.numeric(str_split_i(., " of ", 1))/as.numeric(str_split_i(., " of ", 2)) )
+  mutate_at(vars(count), ~ as.numeric(str_split_i(., " of ", 1))/as.numeric(str_split_i(., " of ", 2)) ) %>%
+  mutate_at(vars(time), ~ as.numeric(hms(.), units="mins") )
 
 View(fight)
 
-
+fight$test <- as.numeric(str_split_i(fight_raw$R_SIG_STR., " of ", 1))/as.numeric(str_split_i(., " of ", 2))
 
 # sample test
 set.seed(2024)
@@ -36,11 +38,10 @@ part_df <- fight %>%
   slice_sample(n=100)
 
 # FAMD
-exc <- c('R_SIG_STR_pct', 'B_SIG_STR_pct') # supplementary variable, which is not included in the analysis
+exc <- c('R_SIG_STR_pct', 'B_SIG_STR_pct', 'R_TD_pct', 'B_TD_pct', 'R_REV', 'B_REV') # supplementary variable, which is not included in the analysis
 res.famd <- part_df %>%
   FAMD(ncp = 10, 
        sup.var = var(exc),  
        graph = TRUE)
 
 fviz_screeplot(res.famd)
-
