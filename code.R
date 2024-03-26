c42feb32347894cd7f3e12d6907e729855028a48
jp83829 <162356282+jp83829@users.noreply.github.com>
2024-03-26 15:52:33 -0700

    Update code.R

--- code.R
+++ code.R
@@ -22,11 +22,14 @@ fighter <- fighter_raw %>%
   select(name, date_of_birth)
 
 # standardize variables
-count <- c('R_SIG_STR.', 'B_SIG_STR.', 'R_TOTAL_STR.', 'B_TOTAL_STR.', 'R_TD', 'B_TD') #character "x of y" to be converted to percentage
+count <- c('R_SIG_STR.', 'B_SIG_STR.', 'R_TOTAL_STR.', 'B_TOTAL_STR.', 'R_TD', 
+           'B_TD', "R_HEAD", "B_HEAD", "R_BODY", "B_BODY", "R_LEG", "B_LEG",
+           "R_DISTANCE", "B_DISTANCE", "R_CLINCH", "B_CLINCH", "R_GROUND", 
+           "B_GROUND") #character "x of y" to be converted to percentage
 time <- c('R_CTRL', 'B_CTRL') #character time span to be converted to numeric
 fight <- fight_raw %>% 
   mutate_at(vars(count), ~ as.numeric(str_split_i(., " of ", 1))/as.numeric(str_split_i(., " of ", 2)) ) %>%
-  mutate_at(vars(time), ~ as.numeric(hms(.), units="mins") )
+  mutate_at(vars(time), ~ as.numeric(str_split_i(., ":", 1))+as.numeric(str_split_i(., ":", 2))/60)
 
 View(fight)
 
@@ -38,7 +41,8 @@ part_df <- fight %>%
   slice_sample(n=100)
 
 # FAMD
-exc <- c('R_SIG_STR_pct', 'B_SIG_STR_pct', 'R_TD_pct', 'B_TD_pct', 'R_REV', 'B_REV') # supplementary variable, which is not included in the analysis
+exc <- c('R_SIG_STR_pct', 'B_SIG_STR_pct', 'R_TD_pct', 'B_TD_pct', 'R_REV', 
+         'B_REV', 'last_round_time') # supplementary variable, which is not included in the analysis
 res.famd <- part_df %>%
   FAMD(ncp = 10, 
        sup.var = var(exc),  
