54c9061b4c6b88f340d22ab52e2ba902b0726e9c
jp83829 <162356282+jp83829@users.noreply.github.com>
2024-04-04 23:57:30 -0700

    Update code.R

--- code.R
+++ code.R
@@ -27,28 +27,57 @@ age_calc_miss <- function(dob, enddate = Sys.Date(), units = "months", precise =
   }
 
 fighter <- fighter_raw %>%
-  select(name, height_cm, reach_in_cm, stance, date_of_birth) %>%
   mutate(age = floor(age_calc_miss(date_of_birth, units = "years"))) %>%
-  subset(! name %in% c(name[duplicated(name)]))
+  select(name, height_cm, reach_in_cm, stance, age) %>%
+  subset(! name %in% c(name[duplicated(name)])) %>%
+  mutate_at(c("height_cm","age"), ~ifelse(is.na(.), median(.,na.rm=T), .)) %>%
+  mutate_at("reach_in_cm", ~ifelse(is.na(.), height_cm, .)) 
+
+# rename for merging data
+
+fighter_R <- fighter %>%
+  rename_all( list(~paste0("R_", .)))
+
+fighter_B <- fighter %>%
+  rename_all( list(~paste0("B_", .)))
 
 # standardize variables
 count <- c('R_SIG_STR.', 'B_SIG_STR.', 'R_TOTAL_STR.', 'B_TOTAL_STR.', 'R_TD', 
            'B_TD', "R_HEAD", "B_HEAD", "R_BODY", "B_BODY", "R_DISTANCE", "B_DISTANCE") #character "x of y" to be converted to percentage
 time <- c('R_CTRL', 'B_CTRL') #character time span to be converted to numeric
+
 fight <- fight_raw %>% 
-  mutate_at(vars(count), ~ as.numeric(str_split_i(., " of ", 1))/as.numeric(str_split_i(., " of ", 2)) ) %>%
-  mutate_at(vars(time), ~ as.numeric(str_split_i(., ":", 1))+as.numeric(str_split_i(., ":", 2))/60) %>%
+  mutate_at(vars(count),
+            ~ as.numeric(str_split_i(., " of ", 1))/as.numeric(str_split_i(., " of ", 2)) ) %>%
+  mutate_at(vars(time),
+            ~ as.numeric(str_split_i(., ":", 1))+as.numeric(str_split_i(., ":", 2))/60) %>%
   mutate_all(~ifelse(is.nan(.), 0, .)) %>%
   mutate_all(~ifelse(is.na(.), 0, .)) %>%
-  subset(Winner!="") %>%
-  left_join(fighter,
-    by = c("R_fighter" = "name"),
-    suffix = c(".x", ".y"),
-    keep = NULL)
-?left_join
+  left_join(fighter_R,
+            by = c("R_fighter" = "R_name"),
+            suffix = c(".x", ".y"),
+            keep = NULL) %>%
+  left_join(fighter_B,
+            by = c("B_fighter" = "B_name"),
+            suffix = c(".x", ".y"),
+            keep = NULL)   %>%
+  subset(Winner!="" & ! grepl('Women',Fight_type)) %>%
+  mutate_at("Fight_type", ~ifelse(grepl("Flyweight", .), "Flyweight Bout", .)) %>%
+  mutate_at("Fight_type", ~ifelse(grepl("Bantamweight", .), "Bantamweight Bout", .)) %>%
+  mutate_at("Fight_type", ~ifelse(grepl("Featherweight", .), "Featherweight Bout", .)) %>%
+  mutate_at("Fight_type", ~ifelse(grepl("Lightweight", .), "Lightweight Bout", .)) %>%
+  mutate_at("Fight_type", ~ifelse(grepl("Welterweight", .), "Welterweight Bout", .)) %>%
+  mutate_at("Fight_type", ~ifelse(grepl("Middleweight", .), "Middleweight Bout", .)) %>%
+  mutate_at("Fight_type", ~ifelse(grepl("Light Heavyweight", .), "Light Heavyweight Bout", .)) %>%
+  mutate_at("Fight_type", ~ifelse(grepl("Heavyweight", .) & ! grepl("Light", .), "Heavyweight Bout", .)) %>% 
+  
+  subset(Winner!="" & 
+           Fight_type %in% c("Flyweight Bout", "Bantamweight Bout",
+                            "Featherweight Bout", "Lightweight Bout", "Welterweight Bout",
+                            "Middleweight Bout", "Light Heavyweight Bout", "Heavyweight Bout")) 
 
-
-
+unique(fight$Fight_type) 
+?fct_recode
 View(fight)
 skim(fight)
 
@@ -71,7 +100,8 @@ exc <- c('R_SIG_STR_pct', 'B_SIG_STR_pct', 'R_TD_pct', 'B_TD_pct', 'R_REV',
 inc <- c("R_KD", "B_KD", "R_SIG_STR.", "B_SIG_STR.", "R_TOTAL_STR.", "B_TOTAL_STR.", 
          "R_SUB_ATT", "B_SUB_ATT", "R_CTRL", "B_CTRL", "R_HEAD", "B_HEAD", 
          "R_BODY", "B_BODY", "R_DISTANCE", "B_DISTANCE", "win_by", "last_round",
-         "Fight_type", "Winner"
+         "Fight_type", "Winner", "R_height_cm", "R_reach_in_cm", "R_stance", "R_age",
+         "B_height_cm", "B_reach_in_cm", "B_stance", "B_age"
          )
 res.famd <- part_df[, inc] %>%
   FAMD(ncp = 6, 
@@ -128,5 +158,3 @@ num <- c("KD", "SIG_STR.", "TOTAL_STR.", "SUB_ATT", "CTRL", "HEAD",
          "BODY", "DISTANCE", "win_by", "last_round")
 rb_sep[, num] %>% 
   ggpairs()
-
-
