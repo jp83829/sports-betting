d7d2e76c578ca99d85e1c124ad655853c4409ae3
jp83829 <162356282+jp83829@users.noreply.github.com>
2024-04-14 19:00:35 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,14 @@
-install.packages(c("readr", "tidyr", "ggplot2"))
+# install packages
+install.packages("pacman")
+pacman::p_load(
+  rio,         # importing data  
+  here,        # relative file pathways  
+  tidyverse,   # data management and visualization
+  magrittr,
+  FactoMineR,  # FAMD
+  factoextra,  # FAMD
+  skimr,
+  GGally,
+  eeptools,
+  ramify 
+  )
