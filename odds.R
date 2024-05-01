b543205527ddb5d90a68f2e3ae6d8ec969bd85d1
jp83829 <162356282+jp83829@users.noreply.github.com>
2024-05-01 19:39:38 -0400

    Create odds.R

---
+++ odds.R
@@ -0,0 +1,12 @@
+# turn odds into implied probability
+am2ip <- function(podds, nodds){
+  ip_p <- (100 / (abs(podds) + 100)) * 100
+  ip_n <- (abs(nodds) / (abs(nodds) + 100)) * 100
+  
+  house_edge <- (ip_p + ip_n) - 100
+  print(house_edge)
+  if (house_edge <0){
+    print("100% proffitable wager ratio (negative/positive):")
+    print((abs(podds)/100 + 1) / (100/abs(nodds) + 1))
+  }
+}
